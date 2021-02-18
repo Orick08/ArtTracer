dfcb5f4d55ec20428c470a17f94b2ce038ba5199
Erick Amezcua <erickamezcua12@gmail.com>
2021-02-18 13:15:53 -0700

    Add view engine and routes

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//Routes
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+app.listen(port);

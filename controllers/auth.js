b4fa281bd2327771bb1c953e2fa09892837351eb
Erick Amezcua <erickamezcua12@gmail.com>
2021-02-23 21:49:54 -0700

    Add register confirmation with the database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,31 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
+
 exports.register = (req, res) => {
   console.log(req.body);
+
+  const { user, email, password} = req.body;
+
+  db.query('SELECT email FROM users WHERE email = ?', [email], (error, result)=>{
+    if(error){
+      console.log(error);
+      //TODO: Mostrar el error en el front-end
+      return;
+    }
+
+    if(result.length > 0){
+      //TODO: Mostrar mensaje de error en el front-end usuario ya registrado
+      return;
+    }
+
+    
+  })
+
   res.send("Form submited");
 }

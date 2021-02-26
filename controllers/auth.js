be5bdaeeea99200a4e9b5e1c7f7cccee4de1f225
Erick Amezcua <erickamezcua12@gmail.com>
2021-02-26 16:54:28 -0700

    Adds login function in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,3 +41,47 @@ exports.register = (req, res) => {
 
   res.send("Form submited");
 }
+
+exports.login = async (req, res) => {
+  try {
+    const { user, password } = req.body;
+
+    if(!user || !password){
+      //TODO: Mensaje de error si no se subio bien el formulario
+      return res.status(400);
+    }
+
+    db.query('SELECT * FROM usuarios WHERE nick = ?', [user], async (error, results)=>{
+      if(error){
+        //TODO: Error de base de datos
+        console.log(error);
+      }
+
+      if(!results || !(await bcrypt.compare(password, results[0].pass))){
+        //TODO: Error email or password is incorrect
+      }
+      else{
+        const id = results[0].id;
+
+        const token = jwt.sign({ id: id }, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRES_IN
+        });
+
+        const cookieOptions ={
+          expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+          ),
+          httpOnly: true
+        }
+
+        res.cookie('artToken', token, cookieOptions);
+        res.status(200);
+        //TODO: Redirect, successful login
+      }
+    });
+
+
+  } catch (error) {
+    console.log(error);
+  }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const authController = require('./../controllers/auth');
 const Router = express.Router();
 
 Router.post('/register', authController.register);
+Router.post('/login', authController.login);
 
 module.exports = Router;

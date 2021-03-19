9129d4524bd4df23fd964c5f22c17cebfe03a315
Erick Amezcua <erickamezcua12@gmail.com>
2021-03-19 16:35:48 -0700

    Clean code app.js

--- app.js
+++ app.js
@@ -10,31 +10,18 @@ const app = express();
 const port = 3000;
 
 const publicDirectory = path.join(__dirname, './public');
+
+//express libraries
 app.use(express.static(publicDirectory));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(cookieParser())
 app.set('view engine', 'ejs');
 
-const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
-});
-
-db.connect((error)=>{
-  if(error){
-    console.log(error);
-    return
-  }
-
-  console.log("Mysql connected...");
-})
-
 //Routes
 const indexRouter = require('./routes/index');
 const indexAuth = require('./routes/auth');
+
 app.use('/', indexRouter);
 app.use('/auth', indexAuth);
 

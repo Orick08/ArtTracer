bde4acc7919e0a092727e3b3946fcca62961396c
Erick Amezcua <erickamezcua12@gmail.com>
2021-03-19 21:07:41 -0700

    Adds needCookie middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,18 @@ exports.redirectWithCookie = (req, res, next) =>{
   }
 };
 
+exports.needsCookie = (req, res, next) => {
+  if(req.cookies.artToken){
+    const DECODED_COOKIE = jwt.verify(req.cookies.artToken, process.env.JWT_SECRET, 'HS512');
+    if(DECODED_COOKIE.id){
+      next();
+      return;
+    }
+  }
+
+  res.render('index', {message: "You need to login to see this page!"})
+}
+
 exports.register = (req, res) => {
   console.log(req.body);
 
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const auth = require('../controllers/auth');
 const Router = express.Router();
 
 //Instance in localhost:3000/dashboard/...
-Router.get('/', (req, res) =>{
+Router.get('/', auth.needsCookie,(req, res) =>{
   res.render('dashboard');
 });
 

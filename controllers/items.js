b39599b4ae027563b19b7ee7e35172c4c86288c9
Erick Amezcua <erickamezcua12@gmail.com>
2021-03-21 16:46:20 -0700

    Adds save on db new item controller

--- controllers/items.js
+++ controllers/items.js
@@ -11,6 +11,31 @@ const db = mysql.createConnection({
 exports.newItem = (req, res)=>{
   let data = req.body;
 
-  data.categoria = parseInt(data.categoria);
-  res.json(req.body);
+  data.id_categoria = parseInt(data.categoria);
+  delete data.categoria;
+
+  //Add db user id to the data
+  if(req.cookies.artToken){
+    const DECODED_COOKIE = jwt.verify(req.cookies.artToken, process.env.JWT_SECRET, 'HS512');
+    if(DECODED_COOKIE.id){
+      data.id_usuario = DECODED_COOKIE.id;
+    }else{
+      res.render('index', {message: "You need to login to see this page!"});
+      return;
+    }
+  }else{
+    res.render('index', {message: "You need to login to see this page!"});
+    return;
+  }
+
+  data.inicio = new Date(data.inicio).toISOString().slice(0, 10).replace('T', ' ');;
+  data.final = new Date(data.final).toISOString().slice(0, 10).replace('T', ' ');;
+
+  db.query('INSERT INTO items SET ?', data, (error, results) =>{
+    if(error){
+      console.log(error);
+    }
+
+    res.redirect('/dashboard');
+  });
 }
